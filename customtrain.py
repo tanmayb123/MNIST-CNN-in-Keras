22fd716557914a79b8cd217c91cf235afdfe28cd
Tanmay bakshi <tanmaybakshi@Tanmays-iMac.local>
2017-04-13 00:13:47 -0400

    Initial

---
+++ customtrain.py
@@ -0,0 +1,63 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense
+
+img_width, img_height = 28, 28
+
+train_data_dir = 'data/train'
+
+validation_data_dir = 'data/validation'
+
+train_samples = 60000
+
+validation_samples = 10000
+
+epoch = 30
+
+# ** Model Begins **
+model = Sequential()
+model.add(Convolution2D(16, 5, 5, activation='relu', input_shape=(img_width, img_height, 3)))
+model.add(MaxPooling2D(2, 2))
+
+model.add(Convolution2D(32, 5, 5, activation='relu'))
+model.add(MaxPooling2D(2, 2))
+
+model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+
+model.add(Dense(10, activation='softmax'))
+# ** Model Ends **
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical')
+
+model.fit_generator(
+        train_generator,
+        samples_per_epoch=train_samples,
+        nb_epoch=epoch,
+        validation_data=validation_generator,
+        nb_val_samples=validation_samples)
+
+model.save_weights('mnistneuralnet.h5')
---
+++ predict.py
@@ -0,0 +1,43 @@
+import numpy
+import cv2
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense
+import sys
+
+img_width, img_height = 28, 28
+
+def create_model():
+  model = Sequential()
+
+  model.add(Convolution2D(16, 5, 5, activation='relu', input_shape=(img_width, img_height, 3)))
+  model.add(MaxPooling2D(2, 2))
+
+  model.add(Convolution2D(32, 5, 5, activation='relu'))
+  model.add(MaxPooling2D(2, 2))
+
+  model.add(Flatten())
+  model.add(Dense(1000, activation='relu'))
+
+  model.add(Dense(10, activation='softmax'))
+
+  model.summary()
+
+  return model
+
+
+img = cv2.imread(sys.argv[1])
+img = cv2.resize(img, (img_width, img_height))
+model = create_model()
+model.load_weights('./mnistneuralnet.h5')
+arr = numpy.array(img).reshape((img_width,img_height,3))
+arr = numpy.expand_dims(arr, axis=0)
+prediction = model.predict(arr)[0]
+bestclass = ''
+bestconf = -1
+for n in [0,1,2,3,4,5,6,7,8,9]:
+	if (prediction[n] > bestconf):
+		bestclass = str(n)
+		bestconf = prediction[n]
+print 'I think this digit is a ' + bestclass + ' with ' + str(bestconf * 100) + '% confidence.'
